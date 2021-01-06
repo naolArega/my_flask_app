3f4d03de2d10dac8bea5a0ce5617980973f9f601
naolArega <na408498@gmail.com>
2021-01-06 19:44:13 +1345

    init commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+	articles = [
+		{
+			'id' : 1,
+			'title' : 'Article One',
+			'body' : 'lorem bla bla bla',
+			'author' : 'naol arega',
+			'created_date' : '04-25-2017'
+		},
+		{
+			'id' : 2,
+			'title' : 'Article Two',
+			'body' : 'lorem bla bla bla',
+			'author' : 'naol arega',
+			'created_date' : '04-25-2017'
+		},
+		{
+			'id' : 3,
+			'title' : 'Article Three',
+			'body' : 'lorem bla bla bla',
+			'author' : 'naol arega',
+			'created_date' : '04-25-2017'
+		}
+	]
+	return articles
